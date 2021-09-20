8d157827b4b8a4c3db2272ec67c3f1bb11cb33fb
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2021-09-20 15:54:39 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,33 +3,36 @@ const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-let degSecond = 360/60;         // un tour de cercle fait 360 degré divisé par 60 secondes(pour un tour entier pour l'aiguille des secondes)
-let degMinute = 360/60/60 ;     //  un tour entier de l'aiguille des minutes se fait en 60 fois de l'aiguille des secondes
-let degHour = 360/12/60/60;     // un tour entier de l'aiguille des heures se fait en 12 fois 
+let degSecondPerSec = 360/60;         // nombre de degré par seconde pour l'aiguille des secondes
+let degMinutePerSec = 360/60/60 ;     // nombre de degré par seconde pour l'aiguille des minutes
+let degHourPerSec = 360/12/60/60;     // nombre de degré par seconde pour l'aiguille des heures
 
+//Extraire l'heure actuelle à l'aide de l'objet Date()
+let today = new Date;
+
+//Stocker l'heure , minute , seconde  dans des varaiables
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+
+let timeInSeconds = second + (minute * 60)+ (hour*3600); // Ici l'heure actuelle en seconde (Depuis 00h00).
+
+let degreTotalSecond = timeInSeconds*degSecondPerSec;   // nombre total de degrés depuis 00h00 de l'aiguille des secondes
+let degreTotalMinute = timeInSeconds*degMinutePerSec;   // de l'aiguille des minutes
+let degreTotalHour   = timeInSeconds*degHourPerSec;     // de l'aiguille des heures
 
 
 // Déplacer les aiguilles 
 function demarrerLaMontre() {
-    //Extraire l'heure actuelle à l'aide de l'objet Date()
-    let today = new Date;
 
-    //Stocker l'heure , minute , seconde  dans des varaiables
-    let hour = today.getHours();
-    let minute = today.getMinutes();
-    let second = today.getSeconds();
+    degreTotalSecond += degSecondPerSec;
+    degreTotalMinute += degMinutePerSec;
+    degreTotalHour   += degHourPerSec;
+    // Pour chaque aiguille, on ajoute le nombre de degré calculé par seconde au précedent nombre de degré total.
 
-
-
-    // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-    // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-    let timeInSeconds = second + (minute * 60)+ (hour*3600); // Ici l'heure actuelle en seconde (Depuis 00h00).
-
-    AIGUILLESEC.style.transform = "rotate(" + timeInSeconds*degSecond + "deg)";
-    AIGUILLEMIN.style.transform = 'rotate(' + timeInSeconds*degMinute + 'deg)';
-    AIGUILLEHR.style.transform  = 'rotate(' + timeInSeconds*degHour + 'deg)';
-
-    // Pour chaque aiguille, on multiplie l'heure actuelle en seconde(timeInSeconds) par le nombre de degré d'avancement de chaque aiguille.
+    AIGUILLESEC.style.transform = "rotate(" + degreTotalSecond + "deg)";
+    AIGUILLEMIN.style.transform = 'rotate(' + degreTotalMinute + 'deg)';
+    AIGUILLEHR.style.transform  = 'rotate(' + degreTotalHour + 'deg)';
     // Et on applique le transform rotate en CSS du nombre de degrés calculés
 
 }
